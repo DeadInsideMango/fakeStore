8eed02181663850f92cde39b42a75fc458b545f4
Roman Hairulin <romanhajrulin67@gmail.com>
2024-03-02 12:19:23 +0200

    git commit

--- src/main/java/com/example/fakeStore/services/ProductService.java
+++ src/main/java/com/example/fakeStore/services/ProductService.java
@@ -4,28 +4,27 @@ import com.example.fakeStore.repository.ProductsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class ProductService implements ProductsRepository {
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public List<Object> getAllProducts() {
         String url = "https://fakestoreapi.com/products";
-        RestTemplate restTemplate = new RestTemplate();
-        Object[] products = restTemplate.getForObject(url, Object[].class);
-        if(products != null) return Arrays.asList(products);
-        else return null;
+        Object[] allProducts = restTemplate.getForObject(url, Object[].class);
+        if(allProducts == null) return Collections.emptyList();
+        return List.of(allProducts);
     }
 
     @Override
     public List<Object> getProductById(String id) {
         String url = "https://fakestoreapi.com/products/".concat(id);
-        RestTemplate restTemplate = new RestTemplate();
-        Object product = restTemplate.getForObject(url, Object.class);
-        if (product != null) return List.of(product);
-        else return null;
+        Object singleProduct = restTemplate.getForObject(url, Object.class);
+        if (singleProduct == null) return Collections.emptyList();
+        return List.of(singleProduct);
     }
 
 }
